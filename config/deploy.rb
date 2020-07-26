049ed8e9117024721595d26bed149a0d3df5af16
yurika-shio <yyurii20tt@gmail.com>
2020-07-26 06:23:18 +0000

    deploy.rb_fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "post-bugs"
-set :repo_url, "https://github.com/yurika-shio/post-bugs.git"
+set :repo_url, "git@github.com:yurika-shio/post-bugs.git"
 set :deploy_to, "/home/ec2-user/post-bugs"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
