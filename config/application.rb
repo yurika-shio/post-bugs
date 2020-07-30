b1ab17e0bf027be07eaf9445de2b6ab4cb496eb1
yurika-shio <yyurii20tt@gmail.com>
2020-07-30 10:43:37 +0000

    日本語対応_add

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PostBugs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.available_locales = :en
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +18,6 @@ module PostBugs
     # the framework and any gems in your application.
   end
 end
+
+
+
---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+I18n.config.available_locales = :ja
+
+# I18n.default_locale = :jas
